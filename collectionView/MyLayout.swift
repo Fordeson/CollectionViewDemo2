13de87996808108baea97d23884d3ac4a37d8f75
王荣荣 <fordesonwang@imingxiang.cn>
2016-07-11 15:36:10 +0800

    修改

---
+++ collectionView/MyLayout.swift
@@ -0,0 +1,66 @@
+//
+//  MyLayout.swift
+//  collectionView
+//
+//  Created by 王荣荣 on 7/11/16.
+//  Copyright © 2016 王荣荣. All rights reserved.
+//
+
+import UIKit
+
+protocol MyLayoutDelegate {
+    func collectionView(collectionView: UICollectionView, heightForItemAtIndexPath indexPath: NSIndexPath) -> CGFloat
+}
+
+class MyLayout: UICollectionViewLayout {
+    private var layoutAttrs = [UICollectionViewLayoutAttributes]()
+    var numberOfColumns = 2
+    private var columnHeights = [CGFloat]()
+    private var contentHeight: CGFloat = 0
+    var  cellPadding: CGFloat = 10
+    var delegate: MyLayoutDelegate!
+    override func prepareLayout() {
+        self.columnHeights = [CGFloat](count: numberOfColumns, repeatedValue: 0)
+        for item in 0..<collectionView!.numberOfItemsInSection(0) {
+            let indexPath = NSIndexPath(forItem: item, inSection: 0)
+            let attr = self.layoutAttributesForItemAtIndexPath(indexPath)
+            self.layoutAttrs.append(attr!)
+        }
+    }
+    
+    override func collectionViewContentSize() -> CGSize {
+        return CGSizeMake(0, self.contentHeight)
+    }
+    
+    override func layoutAttributesForElementsInRect(rect: CGRect) -> [UICollectionViewLayoutAttributes]? {
+        return self.layoutAttrs
+    }
+    
+    override func layoutAttributesForItemAtIndexPath(indexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        let attr = UICollectionViewLayoutAttributes(forCellWithIndexPath: indexPath)
+        let width = (self.collectionView?.frame.size.width)! - (collectionView?.contentInset.left)! - (collectionView?.contentInset.right)!
+        let columnWidth = width / CGFloat(numberOfColumns)
+        let cellHeight = self.delegate.collectionView(collectionView!, heightForItemAtIndexPath: indexPath)
+        var destColumn = 0
+        var minColumnHeight = self.columnHeights[0]
+        for index in 0..<self.columnHeights.count {
+            let columnHeight = self.columnHeights[index]
+            if minColumnHeight > columnHeight {
+                minColumnHeight = columnHeight
+                destColumn = index
+            }
+        }
+        
+        let cellWidth = columnWidth - cellPadding
+        let cellX = CGFloat(destColumn) * columnWidth + cellPadding
+        let cellY = minColumnHeight + cellPadding
+        attr.frame = CGRectMake(cellX, cellY, cellWidth, cellHeight)
+        self.columnHeights[destColumn] = CGRectGetMaxY(attr.frame)
+        let newContentHeight = self.columnHeights[destColumn]
+        if self.contentHeight < newContentHeight {
+            self.contentHeight = newContentHeight
+        }
+        return attr
+    }
+
+}
---
+++ collectionView/ViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  collectionView
+//
+//  Created by 王荣荣 on 7/11/16.
+//  Copyright © 2016 王荣荣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private var images = [UIImage]()
+    private let cellIdentifier = "cellIdentifier"
+    
+    private var layout: MyLayout!
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        self.collectionView.dataSource = self
+        for index in 1...100 {
+            let imageName = "images (\(index)).jpg"
+            let image = UIImage(named: imageName)
+            self.images.append(image!)
+        }
+        
+        if let layout = collectionView.collectionViewLayout as? MyLayout {
+            self.layout = layout
+            layout.numberOfColumns = 4
+             self.collectionView.contentInset = UIEdgeInsetsMake(0, 0, 0, layout.cellPadding)
+            layout.delegate = self
+        }
+    }
+
+}
+
+extension ViewController: UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.images.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(self.cellIdentifier, forIndexPath: indexPath) as! MyCell
+        cell.imageView.image = self.images[indexPath.item]
+        return cell
+    }
+}
+
+extension ViewController: MyLayoutDelegate {
+    func collectionView(collectionView: UICollectionView, heightForItemAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        
+        let image = self.images[indexPath.item]
+        let currentWidth = (collectionView.frame.size.width - CGFloat(layout.numberOfColumns + 1) * layout.cellPadding) / CGFloat(layout.numberOfColumns)
+        let scale = image.size.width / currentWidth
+        let height = image.size.height / scale
+        return height
+    }
+}
+
